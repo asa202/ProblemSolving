6f04ed6085e81631da079f6650b17f0b6cd6724c
asa202 <asa202@sfu.ca>
2021-07-26 15:28:16 +0100

    Brute Force Recursive

---
+++ Min Number Of Jumps/MinNoOfJumps.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+//brute force 
+void helper(vector<int> array, int &min, int idx, int jumps){
+	if(idx >= array.size()-1){
+		if(min > jumps){
+			min = jumps;
+		}
+		return;
+	}else{
+		if(array[idx] == 1){
+			helper(array, min, idx+1, jumps+1);
+		}else{
+			for(int i = 1; i <=array[idx]; i++){
+				helper(array, min, idx+i, jumps+1);
+			}
+		}
+	}
+}
+
+int minNumberOfJumps(vector<int> array) {
+  int min = INT_MAX;
+	helper(array, min, 0, 0);
+  return min;
+}
