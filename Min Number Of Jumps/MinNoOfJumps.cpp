3d22d173e6290c7718fba5b34e7db5351a60a28b
asa202 <asa202@sfu.ca>
2021-08-04 15:28:16 +0100

    linear time and constant space soln

--- Min Number Of Jumps/MinNoOfJumps.cpp
+++ Min Number Of Jumps/MinNoOfJumps.cpp
@@ -1,36 +1,20 @@
 #include <vector>
-#include <climits>
 using namespace std;
-//brute force 
-// void helper(vector<int> array, int &min, int idx, int jumps){
-// 	if(idx >= array.size()-1){
-// 		if(min > jumps){
-// 			min = jumps;
-// 		}
-// 		return;
-// 	}else{
-// 		if(array[idx] == 1){
-// 			helper(array, min, idx+1, jumps+1);
-// 		}else{
-// 			for(int i = 1; i <=array[idx]; i++){
-// 				helper(array, min, idx+i, jumps+1);
-// 			}
-// 		}
-// 	}
-// }
-
+//O(n) time | O(1) time
 int minNumberOfJumps(vector<int> array) {
-	// int min = INT_MAX;
-	// helper(array, min, 0, 0);
-	//O(n^2) time | O(n) space
-	vector<int> jumps(array.size(), INT_MAX);
-	jumps[0] = 0;
-	for(int i = 1 ; i < array.size() ; i++){
-		for(int j = 0; j < i; j++){
-			if(array[j]+j >= i){
-				jumps[i] = std::min(jumps[i],jumps[j]+1);
-			}
+	if(array.size() <=1){
+		return 0;
+	}
+	int jumps = 0;
+	int steps = array[0];
+	int maxReach = array[0];
+	for(int i = 1; i < array.size()-1; i++){
+		maxReach = max(maxReach, i+array[i]);
+		steps--;
+		if(steps == 0){
+			jumps++;
+			steps = maxReach - i;
 		}
 	}
-  return jumps[array.size()-1];
+  return jumps+1;
 }
