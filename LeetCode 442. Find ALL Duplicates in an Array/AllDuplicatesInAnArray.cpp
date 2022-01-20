8aaff1c01058edf34508d2e46aaacd3f1b35d20a
asa202 <asa202@sfu.ca>
2022-01-20 19:48:16 +0100

    Solved in linear time and constant space

--- LeetCode 442. Find ALL Duplicates in an Array/AllDuplicatesInAnArray.cpp
+++ LeetCode 442. Find ALL Duplicates in an Array/AllDuplicatesInAnArray.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<bool> tracker(nums.size()+1,false);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         vector<int> ret;
-        for(int i = 0; i < nums.size(); i++){
-            if(tracker[nums[i]]){
-                ret.push_back(nums[i]);
-            }else{
-                tracker[nums[i]] = true;
-            }
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[abs(nums[i])-1] < 0)ret.push_back(abs(nums[i]));
+            nums[abs(nums[i])-1] *=-1;
         }
         return ret;
     }
