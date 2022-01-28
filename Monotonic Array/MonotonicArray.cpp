335636c55eabe6b8cab4bd9e734c3520ee6cebed
asa202 <asa202@sfu.ca>
2022-01-28 20:28:19 +0100

    Solved Monotonic array
    O(n)time | constant space

---
+++ Monotonic Array/MonotonicArray.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+
+bool isMonotonic(vector<int> array) {
+  // Write your code here.
+	bool flag_in = false;
+	bool flag_dec = false;
+	if(array.size() <= 1){
+		return true;
+	}
+	for(int i = 0 ; i <= array.size()-2 ; i++){
+		if(array[i] > array[i+1]){
+			if(flag_in == false){
+				flag_dec = true;
+			}
+			else{
+				return false;
+			}
+		}
+		else if(array[i] < array[i+1]){
+			if(flag_dec == false){
+				flag_in = true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
+  return true;
+}
