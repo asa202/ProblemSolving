e549248425d4c8793e8bf57365c842c066f9880f
asa202 <asa202@sfu.ca>
2022-03-22 22:29:32 -0700

    dp soln in linear time and constant space

--- Water Area/WaterArea.cpp
+++ Water Area/WaterArea.cpp
@@ -1,26 +1,27 @@
 #include <vector>
 using namespace std;
-//O(n) time | O(n) space
+//O(n) time | O(1) space
 int waterArea(vector<int> heights) {
-  vector<int> maxes(heights.size(), 0);
-	int leftMax = 0;
-	for(int i = 0; i < heights.size() ; i++){
-		maxes[i] = leftMax;
-		leftMax = max(leftMax, heights[i]);
+  // Write your code here.
+	if(heights.size() == 0){
+		return 0;
 	}
-	int rightMax = 0;
-	for(int i = heights.size()-1 ; i >= 0 ; i--){
-		int waterLevel = min(rightMax,maxes[i]);
-		if(waterLevel > heights[i]){
-			maxes[i] = waterLevel - heights[i];
-		}else{
-			maxes[i] = 0;
-		}
-		rightMax = max(rightMax, heights[i]);
-	}
+	int leftIdx = 0;
+	int rightIdx = heights.size()-1;
+	int leftMax = heights[leftIdx];
+	int rightMax = heights[rightIdx];
 	int waterDots = 0;
-	for(int i: maxes){
-		waterDots += i;
+	while(leftIdx < rightIdx){
+		int width = rightIdx - leftIdx - 1;
+		if(heights[leftIdx] < heights[rightIdx]){
+			leftIdx++;
+			leftMax = max(leftMax, heights[leftIdx]);
+			waterDots += leftMax - heights[leftIdx];
+		}else{
+			rightIdx--;
+			rightMax = max(rightMax, heights[rightIdx]);
+			waterDots += rightMax - heights[rightIdx];
+		}
 	}
   return waterDots;
 }
