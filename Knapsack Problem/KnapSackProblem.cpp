d57953764edb84cd4580154e1e85361483eff406
asa202 <asa202@sfu.ca>
2022-03-24 18:40:19 -0700

    O(nc) time and space soln

--- Knapsack Problem/KnapSackProblem.cpp
+++ Knapsack Problem/KnapSackProblem.cpp
@@ -1,39 +1,33 @@
 #include <vector>
 using namespace std;
-//my own solution without help O(n*2^n) time and space
-int vectorWeightSum(vector<vector<int>> items, vector<int> current){
-	int sum = 0;
-	for(int i = 0; i < current.size(); i++){
-		sum+=items[current[i]][1];
-	}
-	return sum;
-}
-int vectorValueSum(vector<vector<int>> items, vector<int> current){
-	int sum = 0;
-	for(int i = 0; i < current.size(); i++){
-		sum+=items[current[i]][0];
-	}
-	return sum;
-}
+//O(nc) time | O(nc) space where c is capacity
 vector<vector<int>> knapsackProblem(vector<vector<int>> items, int capacity) {
-	vector<vector<int>> permutations= {{}};
-	for(int i = 0 ; i < items.size() ; i++){
-		int size = permutations.size();
-		for(int j = 0 ; j < size ;j++){
-			vector<int> current = permutations[j];
-			current.push_back(i);
-			if(vectorWeightSum(items,current) <= capacity){
-				permutations.push_back(current);
+  vector<vector<int>> knapsackValues(items.size()+1, vector<int>(capacity+1, 0));
+	for(int i = 1 ; i < items.size()+1 ; i++){
+		int currentWeight = items[i-1][1];
+		int currentValue = items[i-1][0];
+		for(int j = 0; j < capacity + 1; j++){
+			if(currentWeight > j){
+				knapsackValues[i][j] = knapsackValues[i-1][j];
+			}else{
+				knapsackValues[i][j] = max(knapsackValues[i-1][j],knapsackValues[i-1][j-currentWeight]+currentValue);
 			}
 		}
 	}
+	vector<int> ret{};
 	int max = 0;
-	vector<int> ret = {};
-	for(auto it: permutations){
-		int currSum = vectorValueSum(items,it);
-		if(currSum > max){
-			max = currSum;
-			ret = it;
+	int j = capacity;
+	for(int i = items.size() ; i > 0 ; ){
+		if(knapsackValues[i][j] != knapsackValues[i-1][j]){
+			ret.push_back(i-1);
+			i = i-1;
+			j = j-items[i][1];
+			max += items[i][0];
+		}
+		else if(j==0){
+			break;
+		}else{
+			i = i-1;
 		}
 	}
   return {
